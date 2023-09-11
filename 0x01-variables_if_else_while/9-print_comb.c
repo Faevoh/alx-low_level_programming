ebb75cbcf9f97e94a3a03f22d130db5bac668ca1
Faevoh <favourigbani29@gmail.com>
2023-09-11 06:51:57 -0700

    Print Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
