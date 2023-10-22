31534635464dc3f036a78dbf88a8cf5152f4f6e0
Faevoh <favourigbani29@gmail.com>
2023-10-22 10:48:36 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - takes a pointer and updates value
+  * @n: value placeholder
+  * Return: 0
+  */
+
+void reset_to_98(int *n)
+{
+	int reset;
+
+	reset = 98;
+
+	*n = reset;
+}
