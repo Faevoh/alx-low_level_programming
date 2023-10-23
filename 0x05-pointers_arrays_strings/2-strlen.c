88b43922b8634d28009652a4086149cc36e3f6a9
Faevoh <favourigbani29@gmail.com>
2023-10-23 03:22:26 -0700

    String Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string
+  * @s: string placeholder
+  * Return: 0
+  */
+
+int _strlen(char *s)
+{
+	int char_counter;
+	
+	char_counter = 0;
+	while (s[char_counter] != '\0')
+	{
+		char_counter++;
+	}
+	return (char_counter);
+}
