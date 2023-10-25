b3859464a115e0ace4cb30670777a4119a93dad5
Faevoh <favourigbani29@gmail.com>
2023-10-24 19:26:36 -0700

    Prints all inputs

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - print all arguments it recieves
+  *@argc: number of command line argument
+  *@argv: array of argument
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = o; j < argc; j++)
+	{
+		printf("%s\n", argv[j]);
+	}
+	return (0);
+}
