18911594b52237f034f42798d6b643df14fbaaa8
Faevoh <favourigbani29@gmail.com>
2023-10-25 08:28:19 -0700

    String concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,10 @@ char *str_concat(char *s1, char *s2)
 	size_t len_1, len_2, a, final;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 
 	len_1 = 0;
 	while (s1[len_1] != '\0')
