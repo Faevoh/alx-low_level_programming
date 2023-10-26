eed41318bbfa3c9432cef2137c80330349ee6547
Faevoh <favourigbani29@gmail.com>
2023-10-26 06:05:28 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	else if (new_size =+ old_size)
+	else if (new_size == old_size)
 	{
 		return (ptr);
 	}
