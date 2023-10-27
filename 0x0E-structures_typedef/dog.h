d20e0783888c00c8f18424601d8716d323361e3e
Faevoh <favourigbani29@gmail.com>
2023-10-27 02:26:49 -0700

    Task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  *struct dog - a struct to create a dog info
+  *@name: string variable of dog name
+  *@age: float variable of dog age
+  *@owner: string variable of dog owner
+  *
+  *Description: struct for a dog info
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
