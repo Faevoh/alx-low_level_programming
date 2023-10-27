d6a2f278e9448631b9aaf4033a21c8f9e5988707
Faevoh <favourigbani29@gmail.com>
2023-10-27 03:24:42 -0700

    Dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,7 @@
   *Description: struct for a dog info
   */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -19,7 +19,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
